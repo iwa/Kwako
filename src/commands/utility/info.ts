4905b56a624c78f665f8e501e6b56e3be2163c84
Pierre <19956672+iwa@users.noreply.github.com>
2020-06-07 23:09:56 +0200

    add info command

---
+++ src/commands/utility/info.ts
@@ -0,0 +1,14 @@
+import { Client, Message } from 'discord.js'
+import utilities from '../../utils/utilities'
+
+module.exports.run = async (bot: Client, msg: Message) => {
+    let iwa = await bot.users.fetch(process.env.IWA);
+    let avatar = iwa.avatarURL({ format: 'png', dynamic: false, size: 256 })
+    utilities.info(msg, avatar)
+};
+
+module.exports.help = {
+    name: 'info',
+    usage: "info",
+    desc: "Show some info about Kwako"
+};
