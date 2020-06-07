34f23ba1704f472d8c482202c5c82ac71a2bf15b
Pierre <19956672+iwa@users.noreply.github.com>
2020-06-07 23:02:54 +0200

    add sonicsays command

---
+++ src/commands/memes/sonicsays.ts
@@ -0,0 +1,30 @@
+import { Client, Message } from 'discord.js'
+import * as ejs from 'ejs'
+import imGenerator from '../../utils/img';
+
+module.exports.run = async (bot: Client, msg: Message, args: string[]) => {
+    if (args.length > 0) {
+        msg.channel.startTyping();
+        let parole = args;
+        let x = parole.join(' ')
+
+        let cdnUrl = process.env.CDN_URL;
+
+        let html = await ejs.renderFile('views/sonicsays.ejs', { x, cdnUrl });
+        let file = await imGenerator(385, 209, html, msg.author.id, 'sonic')
+
+        try {
+            console.log(`info: sonicsays by ${msg.author.tag}`)
+            return msg.channel.send('', { files: [file] })
+                .then(() => { msg.channel.stopTyping(true) });
+        } catch (err) {
+            console.error(err)
+        }
+    }
+};
+
+module.exports.help = {
+    name: 'sonicsays',
+    usage: "sonicsays (text)",
+    desc: "Generate a Sonic Says meme"
+};
