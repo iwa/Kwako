965dff65250b2a2d5fdbc8cff3f5647d840481aa
Pierre <19956672+iwa@users.noreply.github.com>
2020-06-07 23:08:50 +0200

    add music commands

---
+++ src/commands/music/loop.ts
@@ -0,0 +1,12 @@
+import { Client, Message } from 'discord.js'
+const music = require('../../utils/music')
+
+module.exports.run = (bot: Client, msg: Message) => {
+    music.loop(msg);
+};
+
+module.exports.help = {
+    name: 'loop',
+    usage: "loop",
+    desc: "Enable / Disable loop for the current song."
+};
