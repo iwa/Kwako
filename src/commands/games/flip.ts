802c05043ac58b41f2362d62b5231f126b3cb82c
Pierre <19956672+iwa@users.noreply.github.com>
2020-06-07 22:57:36 +0200

    add games commands

---
+++ src/commands/games/flip.ts
@@ -0,0 +1,17 @@
+import { Client, Message } from 'discord.js'
+import utilities from '../../utils/utilities'
+
+module.exports.run = (bot: Client, msg: Message) => {
+    let n = utilities.randomInt(2);
+    if (n == 1)
+        msg.channel.send({ "embed": { "title": ":large_blue_diamond: **Heads**" } })
+    else
+        msg.channel.send({ "embed": { "title": ":large_orange_diamond: **Tails**" } })
+    return console.log(`info: flip coin by ${msg.author.tag}`)
+};
+
+module.exports.help = {
+    name: 'flip',
+    usage: "flip",
+    desc: "Flip a coin"
+};
---
+++ src/commands/games/roll.ts
@@ -0,0 +1,31 @@
+import { Client, Message } from 'discord.js'
+import utilities from '../../utils/utilities'
+
+module.exports.run = (bot: Client, msg: Message, args: string[]) => {
+    if (args.length > 0) {
+        let x = args[0]
+        msg.channel.send({
+            "embed": {
+                "title": `🎲 **${utilities.randomInt(parseInt(x))}**`,
+                "color": 5601658
+            }
+        })
+            .then(() => { console.log(`info: roll (${x}) by ${msg.author.tag}`) })
+            .catch(console.error);
+    } else {
+        msg.channel.send({
+            "embed": {
+                "title": `🎲 **${utilities.randomInt(100)}**`,
+                "color": 5601658
+            }
+        })
+            .then(() => { console.log(`info: roll (100) by ${msg.author.tag}`) })
+            .catch(console.error);
+    }
+};
+
+module.exports.help = {
+    name: 'roll',
+    usage: "roll [number]",
+    desc: "Generates a number between 1 and the number you choose.\n_Rolls up to 100 if a number isn't provided._"
+};
