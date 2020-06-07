4a3fdce2826ca8879d64d0947d719616a757b487
Pierre <19956672+iwa@users.noreply.github.com>
2020-06-07 22:29:45 +0200

    add setconf command

---
+++ src/commands/utility/setconf.ts
@@ -0,0 +1,37 @@
+import { Client, Message } from 'discord.js';
+import { Db } from 'mongodb';
+
+module.exports.run = async (bot: Client, msg: Message, args:string[], db:Db, settings:any) => {
+    if(!msg.member.hasPermission('MANAGE_GUILD'))
+        return msg.delete();
+
+    const [prop, ...value] = args;
+
+    if(!settings.has(msg.guild.id, prop))
+        return msg.reply("This key is not in the configuration.");
+
+    let clearValue = value.join(" ");
+
+    if(prop.includes('Channel')) {
+        let channel = args[args.length - 1];
+        if(channel.startsWith('<#') && channel.endsWith('>')) {
+            channel = channel.slice(2, (channel.length-1))
+            let chan = msg.guild.channels.resolve(channel);
+            if(chan && chan.type == 'text' && chan.viewable)
+                clearValue = channel;
+            else
+                return msg.reply("there's a problem with this channel!")
+        } else
+            return msg.reply('please')
+    }
+
+    settings.set(msg.guild.id, clearValue, prop);
+
+    msg.channel.send(`\`${prop}\` has been changed to: **${value.join(" ")}**`);
+};
+
+module.exports.help = {
+    name: 'setconf',
+    usage: "?ping",
+    desc: "This will send you the ping between Q-Bot and Discord's Servers"
+};
