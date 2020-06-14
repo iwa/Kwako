005a4c4e24c958f14520d2b78d571bf16e7fcf9d
Pierre <19956672+iwa@users.noreply.github.com>
2020-06-14 15:30:15 +0200

    add reactions roles embed commands

---
+++ src/commands/staff/addembed.ts
@@ -0,0 +1,30 @@
+import { Client, Message } from 'discord.js';
+import { Db } from 'mongodb'
+
+module.exports.run = async (bot: Client, msg: Message, args: string[], db: Db) => {
+    if (msg.author.id != process.env.IWA) return;
+    let embed = args.join(' ')
+    embed = JSON.parse(embed)
+
+    let sent = await msg.channel.send(embed)
+
+    if (msg.deletable) {
+        try {
+            await msg.delete()
+        } catch (ex) {
+            console.error(ex)
+        }
+    }
+
+    let embed2 = sent.embeds[0];
+    embed2.setFooter(sent.id);
+
+    await sent.edit(embed2);
+
+    await db.collection('msg').insertOne({ _id: (await sent).id, channel: (await sent).channel.id })
+};
+
+module.exports.help = {
+    name: 'addembed',
+    usage: "?addembed"
+};
---
+++ src/commands/staff/addrole.ts
@@ -0,0 +1,36 @@
+import { Client, Message } from 'discord.js';
+import { Db } from 'mongodb'
+
+module.exports.run = async (bot: Client, msg: Message, args: string[], db: Db) => {
+    if (msg.author.id != process.env.IWA) return;
+    let dbEmbed = await db.collection('msg').findOne({ _id: args[0] })
+    if (!dbEmbed) return msg.reply(":x: > That message doesn't exist!")
+    let embed = await msg.channel.messages.fetch(args[0])
+
+    let emote = args[1]
+    try {
+        embed.react(emote)
+    } catch (ex) {
+        return msg.reply(":x: > Can't react!")
+    }
+
+    let roleID = args[2]
+    let role = msg.guild.roles.fetch(roleID);
+    if (!role) return msg.reply(":x: > That role doesn't exist!")
+
+    if (msg.deletable) {
+        try {
+            await msg.delete()
+        } catch (ex) {
+            console.error(ex)
+        }
+    }
+
+    await db.collection('msg').updateOne({ _id: args[0] }, { $push: { roles: { "id": roleID, "emote": emote } } })
+};
+
+module.exports.help = {
+    name: 'addrole',
+    usage: "?addrole",
+    staff: true
+};
