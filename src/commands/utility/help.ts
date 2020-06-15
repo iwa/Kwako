5ab25a2ab975cdfcd2d15294e191a486688978a2
Pierre <19956672+iwa@users.noreply.github.com>
2020-06-15 14:33:15 +0200

    help index now shows server's prefix

--- src/commands/utility/help.ts
+++ src/commands/utility/help.ts
@@ -1,62 +1,54 @@
-import { Client, Message, Collection } from 'discord.js';
+import { Client, Message, Collection, MessageEmbed } from 'discord.js';
 import { Db } from 'mongodb';
 let commands = new Map();
-let member = {}, mod = {};
+let member = new MessageEmbed();
+let mod = new MessageEmbed();
 
 import * as fs from 'fs';
 
 readDirs()
 setTimeout(() => {
-    member = {
-        "embed": {
-            "title": "__**Commands**__",
-            "description": "Prefix : `?`\nUse `?help (command)` to have more info about a specific command",
-            "color": 3852663,
-            "fields": [
-                {
-                    "name": "**👤 Profile**",
-                    "value": commands.get("profile")
-                },
-                {
-                    "name": "**💕 Actions**",
-                    "value": commands.get("actions")
-                },
-                {
-                    "name": "**🕹 Games**",
-                    "value": commands.get("games")
-                },
-                {
-                    "name": "**🎶 Music**",
-                    "value": commands.get("music")
-                },
-                {
-                    "name": "**🛠 Utility**",
-                    "value": commands.get("utility")
-                },
-            ]
-        }
-    }
-    mod = {
-        "embed": {
-            "title": "**⚔️ Staff**",
-            "description": commands.get("staff"),
-            "color": 4886754
-        }
-    }
+    member.setTitle("__**Commands**__")
+    member.setColor(3852663)
+    member.addFields([
+        {
+            "name": "**👤 Profile**",
+            "value": commands.get("profile")
+        },
+        {
+            "name": "**💕 Actions**",
+            "value": commands.get("actions")
+        },
+        {
+            "name": "**🕹 Games**",
+            "value": commands.get("games")
+        },
+        {
+            "name": "**🎶 Music**",
+            "value": commands.get("music")
+        },
+        {
+            "name": "**🛠 Utility**",
+            "value": commands.get("utility")
+        },
+    ])
+
+    mod.setTitle("**⚔️ Staff**")
+    mod.setDescription(commands.get("staff"))
+    mod.setColor(4886754)
 }, 5000)
 
 module.exports.run = async (bot: Client, msg: Message, args: string[], db: Db, commands: Collection<any, any>, settings:Map<string, Object>) => {
+    let config:any = settings.get(msg.guild.id);
     if (args.length == 1) {
         let cmd = commands.get(args[0]);
         if (!cmd || !cmd.help.usage) return;
         if (cmd.help.staff && !msg.member.hasPermission('MANAGE_GUILD')) return;
 
-        let config:any = settings.get(msg.guild.id);
-
         await msg.channel.send("`Syntax : ( ) is needed argument, [ ] is optional argument`")
         return msg.channel.send(`\`\`\`markdown\n< ${cmd.help.name} >\n\n# Usage\n${config.prefix}${cmd.help.usage}\n\n# Description\n${cmd.help.desc}\`\`\``);
     } else
-        sendHelp(msg);
+        sendHelp(msg, config);
     console.log(`info: help sent to ${msg.author.tag}`)
 }
 
@@ -66,7 +58,9 @@ module.exports.help = {
     desc: "Well... Obviously it sends you the list of the commands"
 };
 
-async function sendHelp(msg: Message) {
+async function sendHelp(msg: Message, config: any) {
+    let memberEmbed = member;
+    memberEmbed.setDescription(`Prefix : \`${config.prefix}\`\nUse \`${config.prefix}help (command)\` to have more info about a specific command`)
     if (msg.member.hasPermission('MANAGE_GUILD'))
         try {
             await msg.author.send(member)
