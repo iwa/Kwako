b29ed0f4c40c5630683b5795eb33408ceffe46ea
Pierre <19956672+iwa@users.noreply.github.com>
2020-06-15 20:48:36 +0200

    add 'dellevelrole' command

---
+++ src/commands/staff/dellevelrole.ts
@@ -0,0 +1,27 @@
+import { Client, Message } from 'discord.js';
+import { Db } from 'mongodb'
+
+module.exports.run = async (bot: Client, msg: Message, args: string[], db: Db, commands:any, settings:Map<string, Object>) => {
+    if (!msg.member.hasPermission('MANAGE_GUILD')) return;
+    if (args.length != 1) return;
+    let guildConf = await db.collection('settings').findOne({ '_id': { $eq: msg.guild.id } });
+    let levelroles:string = guildConf.levelroles ? guildConf.levelroles : "[]";
+    let levelrolesMap:Map<number, string> = new Map(JSON.parse(levelroles));
+
+    if(parseInt(args[0]) < 2 || parseInt(args[0]) > 100)
+        return msg.channel.send(":x: > Please choose a valid level number between 2 and 100!")
+
+    levelrolesMap.delete(parseInt(args[0]))
+
+    levelroles = JSON.stringify([...levelrolesMap]);
+
+    await db.collection('settings').updateOne({ _id: msg.guild.id }, { $set: { levelroles: levelroles }}, { upsert: true })
+
+    return msg.channel.send(`I'll no longer give a role when members achieve the level **${args[0]}**.`);
+};
+
+module.exports.help = {
+    name: 'dellevelrole',
+    usage: "dellevelrole (level number)",
+    staff: true
+};
