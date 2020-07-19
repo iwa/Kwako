5fa74abe5a00c4ddc8d462a9761fb3f9439481a3
Pierre <19956672+iwa@users.noreply.github.com>
2020-07-19 11:32:07 +0200

    fix again mute command

--- src/commands/staff/mute.ts
+++ src/commands/staff/mute.ts
@@ -4,9 +4,10 @@ import staff from '../../utils/staff';
 module.exports.run = (bot: Client, msg: Message, args:string[], db:any, commands:any, guildConf:any) => {
     if ((!msg.member.hasPermission('MANAGE_GUILD')) && (msg.author.id != process.env.IWA && process.env.SUDO === '0')) return;
 
-    if(!guildConf.muteRole) return msg.channel.send(":x: > You haven't configured any muted role!")
+    let muteRole = guildConf.muteRole;
+    if(!muteRole) return msg.channel.send(":x: > You haven't configured any muted role!")
 
-    staff.mute(bot, msg, args, guildConf.muteRole);
+    staff.mute(bot, msg, args, muteRole);
 };
 
 module.exports.help = {
