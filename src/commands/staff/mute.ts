81f795d9a1f7d0185706c805b1aad2bd67a37e52
Pierre <19956672+iwa@users.noreply.github.com>
2020-07-19 11:30:59 +0200

    fix mute command

--- src/commands/staff/mute.ts
+++ src/commands/staff/mute.ts
@@ -1,13 +1,12 @@
 import { Client, Message } from 'discord.js'
 import staff from '../../utils/staff';
 
-module.exports.run = (bot: Client, msg: Message, args:string[], db:any, commands:any, settings:Map<string, Object>) => {
+module.exports.run = (bot: Client, msg: Message, args:string[], db:any, commands:any, guildConf:any) => {
     if ((!msg.member.hasPermission('MANAGE_GUILD')) && (msg.author.id != process.env.IWA && process.env.SUDO === '0')) return;
 
-    let config:any = settings.get(msg.guild.id);
-    if(config.muteRole == null) return msg.channel.send(":x: > You haven't configured any muted role!")
+    if(!guildConf.muteRole) return msg.channel.send(":x: > You haven't configured any muted role!")
 
-    staff.mute(bot, msg, args, config.muteRole);
+    staff.mute(bot, msg, args, guildConf.muteRole);
 };
 
 module.exports.help = {
