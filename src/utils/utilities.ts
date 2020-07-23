526ad7af8c4816d513f16003071b16f45c85ff82
Pierre <19956672+iwa@users.noreply.github.com>
2020-07-23 12:43:44 +0200

    update level system

--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -53,23 +53,23 @@ export default class utilities {
      * @param xp - Total amount of exp points of the user
      */
     static levelInfo(xp: number): { 'level': number, 'current': number, 'max': number } {
-        let x = 25;
+        let x = 100;
         for (let i = 1; i < 50; i++) {
             if (xp < x) {
                 return { 'level': i, 'current': xp, 'max': x }
             }
             xp -= x;
-            x = Math.round(Math.log(Math.pow(x, 2))+(x*1.28))
+            x = Math.round(Math.log(Math.pow(x, 2))+(x*1.12))
         }
         return { 'level': 50, 'current': 1, 'max': 1 }
     }
 
     static expForLevel(level: number): number {
-        let x = 25;
+        let x = 100;
         let xp = 0;
         for (let i = 1; i <= level; i++) {
             xp += x;
-            x = Math.round(Math.log(Math.pow(x, 2))+(x*1.28))
+            x = Math.round(Math.log(Math.pow(x, 2))+(x*1.12))
         }
         return xp;
     }
