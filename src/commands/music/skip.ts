0d9f46b6977166cfd6ef2d67eaa28b7b16afce65
Pierre <19956672+iwa@users.noreply.github.com>
2020-07-23 11:15:18 +0200

    add next alias to skip

--- src/commands/music/skip.ts
+++ src/commands/music/skip.ts
@@ -7,6 +7,7 @@ module.exports.run = (bot: Client, msg: Message) => {
 
 module.exports.help = {
     name: 'skip',
+    aliases: ['next'],
     usage: "skip",
     desc: "Vote to skip the current played song\nThe half of the people in the voice channel needs to voteskip for skipping the song",
     perms: ['EMBED_LINKS', 'CONNECT', 'SPEAK', 'USE_VAD']
