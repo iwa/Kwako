74fc7abc0378f556ee28c220f74bea7f99ce3040
Pierre <19956672+iwa@users.noreply.github.com>
2020-07-25 13:07:38 +0200

    fix level up roles not being given at every levels

--- src/utils/levelCheck.ts
+++ src/utils/levelCheck.ts
@@ -11,8 +11,10 @@ import { Db } from "mongodb";
 export default async function levelCheck (msg: Discord.Message, xp: number, db: Db, exp: number) {
     let before = utilities.levelInfo(xp);
     let after = utilities.levelInfo((xp + exp))
-    if(before.level != after.level && (after.level % 5) === 0) {
-        await msg.reply(`you're now level ${after.level}!🎉🎉`).catch(() => {return})
+    if(before.level != after.level) {
+        if ((after.level % 5) === 0)
+            await msg.reply(`you're now level ${after.level}!🎉🎉`).catch(() => {return});
+
         let guildConf = await db.collection('settings').findOne({ '_id': { $eq: msg.guild.id } });
         let levelroles:string = guildConf.levelroles || "[]";
         let levelrolesMap:Map<number, string> = new Map(JSON.parse(levelroles));
