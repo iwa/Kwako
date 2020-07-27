f5de3a0a7792b45c4f31c143e3233a297d98b7a1
Pierre <19956672+iwa@users.noreply.github.com>
2020-07-27 11:33:42 +0200

    add feature to delete birthday + add some security checks

--- src/commands/profile/setbirthday.ts
+++ src/commands/profile/setbirthday.ts
@@ -2,34 +2,59 @@ import { Client, Message, MessageEmbed } from 'discord.js'
 import { Db } from 'mongodb'
 
 module.exports.run = async (bot: Client, msg: Message, args: string[], db: Db, commands: any, guildConf: any) => {
-    if (args.length == 1) {
-        let content = args[0]
-        if (content.length > 5 || content.length < 3) {
-            return msg.channel.send({ "embed": { "title": `:x: > **Date format is invalid ! Please enter your birthday like that :\n${guildConf.prefix}setbirthday mm/dd**`, "color": 13632027 } });
+    if (args.length !== 1) return msg.channel.send({
+        "embed": {
+          "description": `\`${guildConf.prefix}setbirthday (your birthday, mm/dd)\` to register your birthday in your profile card\n\n\`${guildConf.prefix}setbirthday off\` to remove it`,
+          "color": 11462520
         }
+      })
 
-        let date = new Date(content);
+    let content = args[0]
 
-        if (date) {
-            let dd = String(date.getDate()).padStart(2, '0');
-            let mm = String(date.getMonth() + 1).padStart(2, '0');
-            let today = `${mm}/${dd}`;
-            await db.collection('user').updateOne({ _id: msg.author.id }, { $set: { birthday: today } }, { upsert: true });
-            const embed = new MessageEmbed();
-            embed.setAuthor("Your birthday is now set to: ", msg.author.avatarURL({ format: 'png', dynamic: false, size: 128 }));
-            embed.setTitle(`**${today}**`)
-            embed.setFooter('(mm/dd)')
-            embed.setColor('AQUA')
-
+    if (content === 'off') {
+        await db.collection('user').updateOne({ _id: msg.author.id }, { $set: { birthday: null }}, { upsert: true });
+        if (msg.deletable) {
             try {
-                console.log(`info: birthday of ${msg.author.tag} set on ${today}`)
-                return await msg.channel.send(embed);
-            } catch (err) {
-                console.error(err);
+                await msg.delete()
+            } catch (ex) {
+                console.error(ex)
             }
-        } else
-            return msg.channel.send({ "embed": { "title": ":x: > **Date format is invalid! Please enter your birthday in mm/dd format.", "color": 13632027 } });
+        }
+        return msg.channel.send({
+            "embed": {
+              "author": {
+                "name": "✅ Your birthday has been deleted",
+                "icon_url": msg.author.avatarURL({ format: 'png', dynamic: false, size: 128 })
+              },
+              "color": 2270600
+            }
+        })
+    }
 
+    if (content.length > 5 || content.length < 3) {
+        return msg.channel.send({ "embed": { "title": `:x: > **Date format is invalid ! Please enter your birthday like that :\n${guildConf.prefix}setbirthday mm/dd**`, "color": 13632027 } });
+    }
+
+    let date = new Date(content);
+
+    if (date) {
+        let dd = String(date.getDate()).padStart(2, '0');
+        let mm = String(date.getMonth() + 1).padStart(2, '0');
+        if(dd === 'NaN' || mm === 'NaN') return;
+        let today = `${mm}/${dd}`;
+        await db.collection('user').updateOne({ _id: msg.author.id }, { $set: { birthday: today } }, { upsert: true });
+        const embed = new MessageEmbed();
+        embed.setAuthor("Your birthday is now set to: ", msg.author.avatarURL({ format: 'png', dynamic: false, size: 128 }));
+        embed.setTitle(`**${today}**`)
+        embed.setFooter('(mm/dd)')
+        embed.setColor('AQUA')
+
+        try {
+            console.log(`info: birthday of ${msg.author.tag} set on ${today}`)
+            return await msg.channel.send(embed);
+        } catch (err) {
+            console.error(err);
+        }
     } else
         return msg.channel.send({ "embed": { "title": ":x: > **Date format is invalid! Please enter your birthday in mm/dd format.", "color": 13632027 } });
 };
