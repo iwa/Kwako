024cf8719f3481e74d5a799dee8a5b731d57789a
Pierre <19956672+iwa@users.noreply.github.com>
2020-07-28 18:20:34 +0200

    add 'update in..' cooldown command

---
+++ src/commands/update.ts
@@ -0,0 +1,21 @@
+import { Client, Message } from 'discord.js'
+
+module.exports.run = async (bot: Client, msg: Message, args: string[]) => {
+    if (msg.author.id !== process.env.IWA) return;
+
+    let min = 30;
+    if(args[0])
+        min = parseInt(args[0]);
+
+    await bot.user.setActivity(`⚠️ update in ${min}min`, { type: 'LISTENING' }).catch(console.error);
+    await bot.user.setStatus('idle').catch(console.error);
+
+    setInterval(async () => {
+        min -= 1;
+        await bot.user.setActivity(`⚠️ update in ${min}min`, { type: 'LISTENING' }).catch(console.error);
+    }, 60000);
+};
+
+module.exports.help = {
+    name: 'update'
+};
