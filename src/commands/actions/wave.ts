46e502b18aad6d03b2ecda9bd7e6e20113608053
Pierre <19956672+iwa@users.noreply.github.com>
2020-07-28 19:22:07 +0200

    replace greet by wave

--- src/commands/actions/wave.ts
+++ src/commands/actions/wave.ts
@@ -1,31 +1,31 @@
 import { Client, Message, MessageEmbed } from 'discord.js'
 import utilities from '../../utils/utilities'
 
-let nbGifs = 3;
+let nbGifs = 6;
 let lastGif = 0;
 
 module.exports.run = async (bot: Client, msg: Message) => {
     const embed = new MessageEmbed();
     embed.setColor('#F2DEB0')
-    embed.setDescription(`**<@${msg.author.id}>** is greeting!`)
+    embed.setDescription(`**<@${msg.author.id}>** is waving!`)
 
     let n = utilities.randomInt(nbGifs)
     while (lastGif === n)
         n = utilities.randomInt(nbGifs);
     lastGif = n;
 
-    embed.setImage(`https://${process.env.CDN_URL}/img/greet/${n}.gif`);
+    embed.setImage(`https://${process.env.CDN_URL}/img/wave/${n}.gif`);
 
     return msg.channel.send(embed)
         .then(() => {
-            console.log(`info: greetings by ${msg.author.tag}`);
+            console.log(`info: waving by ${msg.author.tag}`);
         })
         .catch(console.error);
 };
 
 module.exports.help = {
-    name: 'greet',
-    usage: "greet",
-    desc: "Greetings!",
+    name: 'wave',
+    usage: "wave",
+    desc: "Waving!",
     perms: ['EMBED_LINKS']
 };
