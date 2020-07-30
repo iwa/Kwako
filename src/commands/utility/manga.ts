b4164c86b17d5f31ce713552d138bae65b314a29
Pierre <19956672+iwa@users.noreply.github.com>
2020-07-30 10:01:48 +0200

    add selector for 'anime' & 'manga' commands

--- src/commands/utility/manga.ts
+++ src/commands/utility/manga.ts
@@ -5,8 +5,49 @@ const Anilist = new al();
 module.exports.run = (bot: Client, msg: Message, args: string[]) => {
     if (args.length < 1) return;
     let req = args.join(' ');
-    Anilist.search('manga', req, 1, 1).then(async (data: { media: any[]; }) => {
-        let res = data.media[0];
+    Anilist.search('manga', req, 1, 5).then(async (data: { media: any[]; }) => {
+        let sent = await msg.channel.send({
+            "embed": {
+              "title": "🔍",
+              "description": `:one: ${data.media[0].title.romaji}\n:two: ${data.media[1].title.romaji}\n:three: ${data.media[2].title.romaji}\n:four: ${data.media[3].title.romaji}\n:five: ${data.media[4].title.romaji}`,
+              "color": 4886754
+            }
+        });
+        await sent.react('1️⃣'); await sent.react('2️⃣'); await sent.react('3️⃣'); await sent.react('4️⃣'); await sent.react('5️⃣');
+
+        let collected = await sent.awaitReactions((_reaction, user) => (['1️⃣','2️⃣','3️⃣','4️⃣','5️⃣'].includes(_reaction.emoji.name)) && (user.id === msg.author.id), { max: 1, time: 30000 })
+
+        sent.delete();
+        if (collected.first() == undefined) return;
+
+        let emote = collected.first().emoji.name
+
+        let res;
+        switch (emote) {
+            case '1️⃣':
+                res = data.media[0];
+            break;
+
+            case '2️⃣':
+                res = data.media[1];
+            break;
+
+            case '3️⃣':
+                res = data.media[2];
+            break;
+
+            case '4️⃣':
+                res = data.media[3];
+            break;
+
+            case '5️⃣':
+                res = data.media[4];
+            break;
+
+            default:
+                return;
+        }
+
         let info = await Anilist.media.manga(res.id)
         const embed = new MessageEmbed();
         if (info.title.romaji == info.title.english)
@@ -21,10 +62,10 @@ module.exports.run = (bot: Client, msg: Message, args: string[]) => {
         embed.addField("Started on", `${info.startDate.year}/${info.startDate.month}/${info.startDate.day}`, true)
         if (info.endDate.day != null)
             embed.addField("Ended on", `${info.endDate.year}/${info.endDate.month}/${info.endDate.day}`, true)
-        embed.addField("Genres", info.genres, false)
+        embed.addField("Genres", info.genres.toString(), false)
         embed.setColor('BLUE')
         embed.setURL(info.siteUrl)
-        console.log(`info: manga request : '${req}' by ${msg.author.tag}`)
+        console.log(`info: manga request: '${req}' by ${msg.author.tag}`)
         return msg.channel.send(embed)
     }).catch((err: any) => {
         console.error(err)
