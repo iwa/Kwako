436162a6c41eb285d6c8fbb5170385934044d285
Pierre <19956672+iwa@users.noreply.github.com>
2020-08-11 00:14:32 +0200

    fix 'forceskip'

--- src/commands/mod/forceskip.ts
+++ src/commands/mod/forceskip.ts
@@ -1,5 +1,5 @@
 import { Client, Message } from 'discord.js'
-const music = require('../../utils/music')
+import music from '../../utils/music'
 
 module.exports.run = (bot: Client, msg: Message) => {
     if ((!msg.member.hasPermission('MANAGE_GUILD'))) return;
