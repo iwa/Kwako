3f10b392f08934fce8a0630d4eae58ef756eea32
Pierre <19956672+iwa@users.noreply.github.com>
2020-08-14 13:06:44 +0200

    update logger in games commands

--- src/commands/games/flip.ts
+++ src/commands/games/flip.ts
@@ -1,13 +1,14 @@
 import { Client, Message } from 'discord.js'
 import utilities from '../../utils/utilities'
+import { Logger } from 'pino';
 
-module.exports.run = (bot: Client, msg: Message) => {
+module.exports.run = (bot: Client, msg: Message, args: any, db: any, log: Logger) => {
     let n = utilities.randomInt(2);
     if (n == 1)
         msg.channel.send({ "embed": { "title": ":large_blue_diamond: **Heads**" } })
     else
         msg.channel.send({ "embed": { "title": ":large_orange_diamond: **Tails**" } })
-    return console.log(`info: flip coin by ${msg.author.tag}`)
+    return log.info({msg: 'flip', author: { id: msg.author.id, name: msg.author.tag }, guild: msg.guild.id})
 };
 
 module.exports.help = {
