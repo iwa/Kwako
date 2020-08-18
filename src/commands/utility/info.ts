1bd177cc1e9f83d091bf1d75e810273dc1a565b0
Pierre <19956672+iwa@users.noreply.github.com>
2020-08-18 18:15:15 +0200

    update info command

--- src/commands/utility/info.ts
+++ src/commands/utility/info.ts
@@ -1,19 +1,19 @@
 import Kwako from '../../Client'
 import { Message } from 'discord.js'
 
-let packageJson = require('../../package.json')
+let { version } = require('../../../package.json')
 
-module.exports.run = async (msg: Message) => {
+module.exports.run = async (msg: Message, args: any, guildConf: any) => {
     let iwa = await Kwako.users.fetch(process.env.IWA);
     let avatar = iwa.avatarURL({ format: 'png', dynamic: false, size: 256 })
 
     let embed = {
         "embed": {
             "title": "**Bot Infos**",
-            "description": "Kwako is developed and handled by <@125325519054045184>.\n\nLanguage : `TypeScript` using NodeJS\nAPI Access : `discord.js` package\n\nYou can access to the index of commands by typing `?help`\n\nAll my work is done for free, but you can still financially support me [here](https://www.patreon.com/iwaQwQ)",
+            "description": `Kwako is developed and handled by <@125325519054045184>.\n\nLanguage : \`TypeScript\` using NodeJS\nAPI Access : \`discord.js\` package\n\nYou can access to the index of commands by typing \`${guildConf.prefix}help\`\n\nAll my work is done for free, but you can still financially support me [here](https://www.patreon.com/iwa)`,
             "color": 13002714,
             "footer": {
-                "text": `Created with ♥ by iwa | Copyright © iwa, v${packageJson.version}`
+                "text": `Created with ♥ by iwa | Copyright © iwa, v${version}`
             },
             "thumbnail": {
                 "url": avatar
@@ -23,9 +23,9 @@ module.exports.run = async (msg: Message) => {
 
     try {
         Kwako.log.info({msg: 'info', author: { id: msg.author.id, name: msg.author.tag }, guild: { id: msg.guild.id, name: msg.guild.name }});
-        await msg.author.send(embed)
+        await msg.channel.send(embed)
     } catch (ex) {
-        return msg.channel.send("**:x: > Please open your DMs!**")
+        Kwako.log.error(ex)
     }
 };
 
