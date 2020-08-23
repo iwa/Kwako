dc82015d73041f022cad1b682d3c1890a90fe232
Pierre <19956672+iwa@users.noreply.github.com>
2020-08-23 21:50:40 +0200

    add anilistSearch to avoid code repetition

--- src/commands/utility/anime.ts
+++ src/commands/utility/anime.ts
@@ -1,67 +1,14 @@
 import Kwako from '../../Client'
 import { Message, MessageEmbed } from 'discord.js'
+import anilistSearch from '../../utils/anilistSearch';
 const al = require('anilist-node');
 const Anilist = new al();
-
-let number = ['1️⃣','2️⃣','3️⃣','4️⃣','5️⃣'];
 
 module.exports.run = (msg: Message, args: string[]) => {
     if (args.length < 1) return;
     let req = args.join(' ');
     Anilist.search('anime', req, 1, 5).then(async (data: { media: any[]; }) => {
-        let desc = "";
-
-        for(let i = 0; i < data.media.length; i++)
-            desc = `${desc}${number[i]} ${data.media[i].title.romaji}\n`
-
-        let sent = await msg.channel.send({
-            "embed": {
-              "title": "🔍",
-              "description": desc,
-              "color": 4886754
-            }
-        });
-
-        for(let i = 0; i < data.media.length; i++)
-            await sent.react(number[i])
-
-        let collected = await sent.awaitReactions((_reaction, user) => (['1️⃣','2️⃣','3️⃣','4️⃣','5️⃣'].includes(_reaction.emoji.name)) && (user.id === msg.author.id), { max: 1, time: 30000 })
-
-        sent.delete();
-        if (collected.first() == undefined) return;
-
-        let emote = collected.first().emoji.name
-
-        let res;
-        switch (emote) {
-            case '1️⃣':
-                if(!data.media[0]) return;
-                res = data.media[0];
-            break;
-
-            case '2️⃣':
-                if(!data.media[1]) return;
-                res = data.media[1];
-            break;
-
-            case '3️⃣':
-                if(!data.media[2]) return;
-                res = data.media[2];
-            break;
-
-            case '4️⃣':
-                if(!data.media[3]) return;
-                res = data.media[3];
-            break;
-
-            case '5️⃣':
-                if(!data.media[4]) return;
-                res = data.media[4];
-            break;
-
-            default:
-                return;
-        }
+        let res = await anilistSearch(msg, data);
 
         if(!res) return msg.react('❌');
 
