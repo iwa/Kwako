8ac70e49bf643e217ea539f76c47c59b8128fe20
Pierre <19956672+iwa@users.noreply.github.com>
2020-09-01 18:46:21 +0200

    add missing radix parameters

--- src/commands/config/dellevelrole.ts
+++ src/commands/config/dellevelrole.ts
@@ -17,10 +17,10 @@ module.exports.run = async (msg: Message, args: string[]) => {
     let levelroles:string = guildConf.levelroles ? guildConf.levelroles : "[]";
     let levelrolesMap:Map<number, string> = new Map(JSON.parse(levelroles));
 
-    if(parseInt(args[0]) < 2 || parseInt(args[0]) > 100)
+    if(parseInt(args[0], 10) < 2 || parseInt(args[0], 10) > 100)
         return msg.channel.send(":x: > Please choose a valid level number between 2 and 100!")
 
-    levelrolesMap.delete(parseInt(args[0]))
+    levelrolesMap.delete(parseInt(args[0], 10))
 
     levelroles = JSON.stringify([...levelrolesMap]);
 
