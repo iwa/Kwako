aa1429d92c3ef7bd5174cf5410a6ef3d5c08c386
Pierre <iwaongit@gmail.com>
2021-03-20 22:16:47 +0100

    feat: :sparkles: divorce users when one left the server

--- src/commands/marriage/weddings.ts
+++ src/commands/marriage/weddings.ts
@@ -9,10 +9,14 @@ module.exports.run = async (msg: Message) => {
     let text = "";
     for(const user of result) {
         if(!done.has(user._id) && !done.has(user.marriage[msg.guild.id])) {
-            let first = await msg.guild.members.fetch(user._id);
-            let second = await msg.guild.members.fetch(user.marriage[msg.guild.id]);
+            let first = await msg.guild.members.fetch(user._id).catch(() => {return});
+            let second = await msg.guild.members.fetch(user.marriage[msg.guild.id]).catch(() => {return});
 
-            if(!first || !second) return;
+            if(!first || !second) {
+                await Kwako.db.collection('user').updateOne({ _id: user._id }, { $unset: { [guild]: "" } });
+                await Kwako.db.collection('user').updateOne({ _id: user.marriage[msg.guild.id] }, { $unset: { [guild]: "" } });
+                return;
+            }
 
             text = `${text}${first.user.username} 💞 ${second.user.username}\n`;
             done.add(first.id); done.add(second.id);
