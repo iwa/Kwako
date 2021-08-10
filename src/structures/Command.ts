2c91c3f3f555f8c7e0ad357eb3bf290eebb6c9b0
Pierre <iwaongit@gmail.com>
2021-08-10 14:39:50 +0200

    feat: :memo: update Command structure to welcome slash commands

--- src/structures/Command.ts
+++ src/structures/Command.ts
@@ -4,16 +4,21 @@ import PermLevels from "./PermLevels";
 export default class Command {
 
     public readonly name: string;
-    public readonly run: Function;
+    public readonly desc: string;
+    public readonly runMsg: Function;
+    public readonly runInteraction: Function;
 
     public readonly permLevel: PermLevels;
     public readonly aliases: readonly string[];
     public readonly discordPerm: PermissionString[];
     public readonly usage: string;
+    public readonly premium: boolean;
 
-    constructor(name: string, run: Function, permLevel: PermLevels, aliases: string[], discordPerm: PermissionString[], usage: string) {
+    constructor(name: string, desc: string, runMsg: Function, runInteraction: Function, permLevel: PermLevels, aliases: string[], discordPerm: PermissionString[], usage: string) {
         this.name = name;
-        this.run = run;
+        this.desc = desc;
+        this.runMsg = runMsg;
+        this.runInteraction = runInteraction;
         this.permLevel = permLevel;
         this.aliases = aliases || [];
         this.discordPerm = discordPerm || [];
